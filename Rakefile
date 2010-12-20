27777c6ce70d5a4dabc3cea3e05d9ad211c40539
Graham Ashton <graham@effectif.com>
2010-12-20 16:24:03 +0000

    Switch to committing config.yml. Use passenger.

--- Rakefile
+++ Rakefile
@@ -5,7 +5,7 @@ Bundler.require(:default, :test)
 
 begin
   require 'vlad'
-  Vlad.load(:scm => :git, :app => nil, :web => nil)
+  Vlad.load(:scm => :git, :app => :passenger, :web => nil)
 rescue LoadError
 end
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -11,28 +11,12 @@ require "tempfile"
 require "vlad"
 
 namespace :vlad do
-  remote_task :config_yml do
-    put "#{shared_path}/config.yml", "vlad.config.yml" do
-      File.open(File.join(File.dirname(__FILE__), "config.yml")).read
-    end
-  end
-  
-  task :setup do
-    Rake::Task['vlad:config_yml'].invoke
-  end
-  
-  remote_task :symlink_config_yml do
-    run "ln -s #{shared_path}/config.yml #{current_path}/config/config.yml"
-  end
-  
   remote_task :symlink_attachments do
     run "ln -s #{shared_path}/content/attachments #{current_path}/public/attachments"
   end
   
   task :update do
-    [:symlink_config_yml, :symlink_attachments].each do |task|
-      Rake::Task["vlad:#{task}"].invoke
-    end
+    Rake::Task["vlad:symlink_attachments"].invoke
   end
   
   # If you use bundler to manage the installation of gems on your server
